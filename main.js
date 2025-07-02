cc8550f1df883e11956c1324d0cee323c436de25
gongjundong <gongjundong66@gmail.com>
2025-07-03 05:58:33 +0700

    Update main.js

--- main.js
+++ main.js
@@ -19,3 +19,31 @@ window.onload = () => {
   switchLanguage(currentLang);
   renderProducts();
 };
+function renderPayPalButtons() {
+  products.forEach(product => {
+    paypal.Buttons({
+      createOrder: function(data, actions) {
+        return actions.order.create({
+          purchase_units: [{
+            amount: {
+              value: product.price
+            },
+            description: product.name.en
+          }]
+        });
+      },
+      onApprove: function(data, actions) {
+        return actions.order.capture().then(function(details) {
+          alert('Thanks, ' + details.payer.name.given_name + '! Your purchase of ' + product.name.en + ' is confirmed.');
+        });
+      }
+    }).render(`#paypal-button-container-${product.id}`);
+  });
+}
+window.onload = () => {
+  switchLanguage(currentLang);
+  renderProducts();
+  renderPayPalButtons();
+};
+card.innerHTML += `<div id="paypal-button-container-${product.id}"></div>`;
+
